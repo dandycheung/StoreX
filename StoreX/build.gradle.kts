d64d5cf54f606bc3e3cec0148ebd23733f6bff13
Romman Sabbir <rommansabbir@gmail.com>
2024-08-19 21:44:39 +0600

    feat : jdk compiler version updated

--- StoreX/build.gradle.kts
+++ StoreX/build.gradle.kts
@@ -15,8 +15,6 @@ android {
     defaultConfig {
         minSdk = 19 // Use 'minSdk' in Kotlin DSL
         targetSdk = 34 // Use 'targetSdk' in Kotlin DSL
-/*        versionCode = 1
-        versionName = "1.0"*/
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
@@ -37,11 +35,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = JavaVersion.VERSION_17.toString()
     }
 }
 
